9597be73c608fe04b391725bda949fffc1b00cd4
github username <andrewcohn0@gmail.com>
2018-01-10 12:40:54 -0500

    minor format changes

--- test/TwitterMessage-test.js
+++ test/TwitterMessage-test.js
@@ -1,30 +1,39 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import React from "react";
+import { expect } from "chai";
+import { shallow } from "enzyme";
 
-import TwitterMessage from '../src/components/TwitterMessage';
+import TwitterMessage from "../src/components/TwitterMessage";
 
-describe('<TwitterMessage />', () => {
-  describe('Saving input value in state', () => {
-    it('should update the state when typing', () => {
+describe("<TwitterMessage />", () => {
+  describe("Saving input value in state", () => {
+    it("should update the state when typing", () => {
       const wrapper = shallow(<TwitterMessage maxChars={140} />);
-      const event = { target: { value: 'f' } };
-      wrapper.find('input').simulate('change', event);
-      expect(wrapper.find('input').props().value).to.deep.equal(event.target.value, 'The input value is not being updated when it changes');
+      const event = { target: { value: "f" } };
+      wrapper.find("input").simulate("change", event);
+      expect(wrapper.find("input").props().value).to.deep.equal(
+        event.target.value,
+        "The input value is not being updated when it changes"
+      );
     });
   });
 
-  describe('Character counter', () => {
-    it('should show the remaining characters counter', () => {
+  describe("Character counter", () => {
+    it("should show the remaining characters counter", () => {
       const wrapper = shallow(<TwitterMessage maxChars={140} />);
-      expect(wrapper.contains(140), 'The maxChars prop is not being rendered in the component').to.be.true;
+      expect(
+        wrapper.contains(140),
+        "The maxChars prop is not being rendered in the component"
+      ).to.be.true;
     });
 
-    it('should update the counter when typing', () => {
+    it("should update the counter when typing", () => {
       const wrapper = shallow(<TwitterMessage maxChars={140} />);
-      const event = { target: { value: 'f' } };
-      wrapper.find('input').simulate('change', event);
-      expect(wrapper.contains(139), 'The character counter does not update when typing').to.be.true;
+      const event = { target: { value: "f" } };
+      wrapper.find("input").simulate("change", event);
+      expect(
+        wrapper.contains(139),
+        "The character counter does not update when typing"
+      ).to.be.true;
     });
   });
 });
